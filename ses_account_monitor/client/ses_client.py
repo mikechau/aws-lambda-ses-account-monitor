6ff570b74ca011b2ca74201a9164b050897eceb9
Mike Chau <mikechau@users.noreply.github.com>
2018-06-17 00:05:35 -0700

    Spike ses client

---
+++ ses_account_monitor/client/ses_client.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import boto3
+
+from ses_account_monitor.config import LOG_LEVEL
+from ses_account_monitor.util import (
+    json_dump_request_event,
+    json_dump_response_event)
+
+
+class SesClient(object):
+    def __init__(self,
+                 client=None,
+                 logger=None,
+                 session_config=None):
+
+        self._session_config = session_config
+        self._set_client(client)
+        self._set_logger(logger)
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def logger(self):
+        return self._logger
+
+    def get_account_sending_quota(self):
+        return self.client.get_send_quota()
+
+    def is_account_sending_rate_over(self, percent=None):
+        if percent is None:
+            percent = 100
+
+        stats = self.get_account_sending_quota()
+
+        threshold = (percent * stats['MaxSendRate']) / 100.0
+
+        return stats['SentLast24Hours'] >= threshold
+
+    def is_account_sending_enabled(self):
+        return self.client.get_account_sending_enabled()['Enabled']
+
+    def toggle_account_sending(self):
+        if self.is_account_sending_enabled():
+            self.logger.debug('SES account sending is currently ENABLED, transitioning to DISABLED.')
+
+            return self.disable_account_sending()
+        else:
+            self.logger.debug('SES account sending is currently DISABLED, transitioning to ENABLED.')
+
+            return self.enable_account_sending()
+
+    def enable_account_sending(self):
+        self._log_enable_account_sending_request()
+
+        self.client.update_account_sending_enabled(Enabled=True)
+
+        self._log_enable_account_sending_response()
+
+        return True
+
+    def disable_account_sending(self):
+        self._log_disable_account_sending_request()
+
+        self.client.update_account_sending_enabled(Enabled=False)
+
+        self._log_disable_account_sending_response()
+
+        return False
+
+    def _set_client(self, client):
+        if client:
+            self._client = client
+        else:
+            self._client = self._build_ses_client()
+
+    def _set_logger(self, logger):
+        if logger:
+            self._logger = logger
+        else:
+            self._logger = logging.getLogger(__name__)
+            self._logger.setLevel(LOG_LEVEL)
+
+    def _build_ses_client(self):
+        session_config = self._session_config
+
+        if session_config:
+            session = boto3.Session(**session_config)
+            client = session.client('ses')
+        else:
+            client = boto3.client('ses')
+
+        return client
+
+    def _log_enable_account_sending_request(self):
+        self.logger.debug('Preparing to enable SES account sending...')
+
+        self.logger.info(
+            json_dump_request_event(class_name=self.__class__.__name__,
+                                    method_name='enable_account_sending_request'))
+
+    def _log_enable_account_sending_response(self):
+        self.logger.debug('SES account sending ENABLED!')
+
+        self.logger.info(
+            json_dump_response_event(class_name=self.__class__.__name__,
+                                     method_name='enable_account_sending_request'))
+
+    def _log_disable_account_sending_request(self):
+        self.logger.debug('Preparing to disable SES account sending...')
+
+        self.logger.info(
+            json_dump_request_event(class_name=self.__class__.__name__,
+                                    method_name='disable_account_sending_request'))
+
+    def _log_disable_account_sending_response(self):
+        self.logger.debug('SES account sending DISABLED!')
+
+        self.logger.info(
+            json_dump_response_event(class_name=self.__class__.__name__,
+                                     method_name='disable_account_sending_request'))
---
+++ test/client/test_ses_client.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import boto3
+import pytest
+
+from botocore.stub import Stubber
+from ses_account_monitor.client.ses_client import SesClient
+
+
+@pytest.fixture()
+def ses_client():
+    return boto3.client('ses',
+                        aws_access_key_id='a',
+                        aws_secret_access_key='b',
+                        region_name='us-west-2')
+
+
+@pytest.fixture()
+def ses_quota_responses():
+    return (({
+        'Max24HourSend': 123.0,
+        'MaxSendRate': 123.0,
+        'SentLast24Hours': 0.0
+    }, 90, False), ({
+        'Max24HourSend': 123.0,
+        'MaxSendRate': 123.0,
+        'SentLast24Hours': 123.0
+    }, 80, True), ({
+        'Max24HourSend': 123.0,
+        'MaxSendRate': 123.0,
+        'SentLast24Hours': 150.0
+    }, 100, True), ({
+        'Max24HourSend': 123.0,
+        'MaxSendRate': 123.0,
+        'SentLast24Hours': 123.0
+    }, None, True))
+
+
+def test_get_account_sending_quota(ses_client, ses_quota_responses):
+    stubber = Stubber(ses_client)
+
+    stubber.add_response('get_send_quota',
+                         ses_quota_responses[0][0],
+                         {})
+    stubber.activate()
+
+    client = SesClient(client=ses_client)
+    results = client.get_account_sending_quota()
+
+    assert results == {
+        'Max24HourSend': 123.0,
+        'MaxSendRate': 123.0,
+        'SentLast24Hours': 0.0
+    }
+
+
+def test_is_account_sending_rate_over(ses_client, ses_quota_responses):
+    stubber = Stubber(ses_client)
+    client = SesClient(client=ses_client)
+
+    for response, percentage, expected_result in ses_quota_responses:
+        stubber.add_response('get_send_quota',
+                             response,
+                             {})
+        stubber.activate()
+
+        results = client.is_account_sending_rate_over(percentage)
+
+        stubber.deactivate()
+
+        assert results == expected_result
+
+
+def test_toggle_account_sending(ses_client):
+    stubber = Stubber(ses_client)
+
+    stubber.add_response('get_account_sending_enabled',
+                         {'Enabled': True},
+                         {})
+    stubber.add_response('update_account_sending_enabled',
+                         {},
+                         {'Enabled': False})
+    stubber.add_response('get_account_sending_enabled',
+                         {'Enabled': False},
+                         {})
+    stubber.add_response('update_account_sending_enabled',
+                         {},
+                         {'Enabled': True})
+
+    with stubber:
+        client = SesClient(client=ses_client)
+
+        disable_response = client.toggle_account_sending()
+        assert disable_response is False
+
+        enable_response = client.toggle_account_sending()
+        assert enable_response is True
+
+
+def test_enable_account_sending(ses_client):
+    stubber = Stubber(ses_client)
+    stubber.add_response('update_account_sending_enabled',
+                         {},
+                         {'Enabled': True})
+    stubber.activate()
+
+    client = SesClient(client=ses_client)
+    response = client.enable_account_sending()
+
+    assert response is True
+
+
+def test_disable_account_sending(ses_client):
+    stubber = Stubber(ses_client)
+    stubber.add_response('update_account_sending_enabled',
+                         {},
+                         {'Enabled': False})
+    stubber.activate()
+
+    client = SesClient(client=ses_client)
+    response = client.disable_account_sending()
+
+    assert response is False
