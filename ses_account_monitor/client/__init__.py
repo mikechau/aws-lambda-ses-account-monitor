f6038472b4040cd59cc912822a77bb48e1f7924c
Mike Chau <mikechau@users.noreply.github.com>
2018-06-17 21:12:05 -0700

    Rename client to clients

--- ses_account_monitor/client/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from ses_account_monitor.client.cloudwatch_client import CloudWatchClient
-from ses_account_monitor.client.pagerduty_client import PagerDutyClient
-from ses_account_monitor.client.ses_client import SesClient
-from ses_account_monitor.client.slack_client import SlackClient
-
-__all__ = ['CloudwatchClient', 'PagerDutyClient', 'SesClient', 'SlackClient']
--- ses_account_monitor/clients/slack_client.py
+++ ses_account_monitor/clients/slack_client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from ses_account_monitor.client.http_client import HttpClient
+from ses_account_monitor.clients.http_client import HttpClient
 
 
 class SlackClient(HttpClient):
