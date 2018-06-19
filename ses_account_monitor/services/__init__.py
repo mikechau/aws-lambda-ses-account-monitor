54fd95e701d2bac7027b7b48948fd531fe665677
Mike Chau <37817052+ellation-mchau@users.noreply.github.com>
2018-06-18 17:30:18 -0700

    Rename pagerduty to pager_duty

--- ses_account_monitor/services/__init__.py
+++ ses_account_monitor/services/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ses_account_monitor.services.cloudwatch_service import CloudWatchService
-from ses_account_monitor.services.pagerduty_service import PagerDutyService
+from ses_account_monitor.services.pager_duty_service import PagerDutyService
 from ses_account_monitor.services.ses_service import SesService
 from ses_account_monitor.services.slack_service import SlackService
 
