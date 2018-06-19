6c580a6315110cc031aae134809ab98810cb9b24
Mike Chau <mikechau@users.noreply.github.com>
2018-06-19 02:01:30 -0700

    Remove setting action from services

--- ses_account_monitor/services/slack_service.py
+++ ses_account_monitor/services/slack_service.py
@@ -32,7 +32,6 @@ class SlackService(HttpClient):
                  url=None,
                  channels=None,
                  config=None,
-                 action=None,
                  dry_run=False,
                  logger=None):
 
@@ -45,7 +44,6 @@ class SlackService(HttpClient):
         super(SlackService, self).__init__(url=url,
                                            logger=logger)
 
-        self.action = (action or self._config.action)
         self.messages = deque([])
         self.channels = (channels or self._config.channels)
 
@@ -210,7 +208,7 @@ class SlackService(HttpClient):
                         },
                         {
                             'title': 'Action',
-                            'value': (action or self.action).upper(),
+                            'value': (action or ACTION_ALERT).upper(),
                             'short': True
                         }
                     ],
