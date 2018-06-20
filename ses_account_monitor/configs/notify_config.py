c8df0a12fe6bb4f3d6646d5ccebbbb57538c432f
Mike Chau <37817052+ellation-mchau@users.noreply.github.com>
2018-06-20 11:47:39 -0700

    Add configs docstrings

--- ses_account_monitor/configs/notify_config.py
+++ ses_account_monitor/configs/notify_config.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
+
+'''
+ses_account_monitor.config.notify_config
+~~~~~~~~~~~~~~~~
+
+Notify config module, flags for when to send event notifications.
+'''
+
 from collections import namedtuple
+
 
 NotifyConfig = namedtuple('NotifyConfig', ('notify_pager_duty_on_ses_reputation',
                                            'notify_pager_duty_on_ses_sending_quota',
                                            'notify_slack_on_ses_reputation',
                                            'notify_slack_on_ses_sending_quota'))
+'''
+class:NotifyConfig
+
+Args:
+    notify_pager_duty_on_ses_reputation (bool): Flag to enable SES reputation notifications to Pager Duty.
+    notify_pager_duty_on_ses_sending_quota (bool): Flag to enable SES sending quota notifications to Pager Duty.
+    notify_slack_on_ses_reputation (bool): Flag to enable SES reputation notifications to Slack.
+    notify_slack_on_ses_sending_quota (bool): Flag to enable SES sending quota notifications to Slack.
+'''
--- ses_account_monitor/configs/slack_service_config.py
+++ ses_account_monitor/configs/slack_service_config.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-import os
+
+'''
+ses_account_monitor.configs.slack_service_config
+~~~~~~~~~~~~~~~~
+
+Slack service config module, context used by the Slack service.
+'''
 
 from collections import namedtuple
 
@@ -13,3 +19,18 @@ SlackServiceConfig = namedtuple('SlackServiceConfig', ('aws_account_name',
                                                        'ses_console_url',
                                                        'ses_reputation_dashboard_url',
                                                        'webhook_url'))
+'''
+class:SlackServiceConfig
+
+Args:
+    aws_account_name (str): AWS account name. Ex: supercoolco.
+    aws_environment (str): AWS environment name. Ex: prod.
+    aws_region (str): AWS region. Ex: us-west-2.
+    channels (list): List of channels to post to. Ex: ['#alerts']
+    footer_icon_url (str/None): Slack footer icon url.
+    icon_emoji (str/None): Slack icon emoji.
+    service_name (str): The name of the service. Ex: lambda-ses-account-monitor.
+    ses_console_url (str): The SES console url.
+    ses_reputation_dashboard_url (str): The SES reputation dashboard url.
+    webhook_url (str): The Slack webhook url to post messages to.
+'''
