b2b9d7a65e318d21dbb513052c63f67f1e4c8638
Mike Chau <37817052+ellation-mchau@users.noreply.github.com>
2018-06-20 11:47:39 -0700

    Add configs docstrings

--- ses_account_monitor/configs/pager_duty_service_config.py
+++ ses_account_monitor/configs/pager_duty_service_config.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+
+'''
+ses_account_monitor.configs.pager_duty_service_config
+~~~~~~~~~~~~~~~~
+
+Pager Duty service config module, context used by the Pager Duty service.
+'''
+
 from collections import namedtuple
+
 
 PagerDutyServiceConfig = namedtuple('PagerDutyServiceConfig', ('aws_account_name',
                                                                'aws_environment',
@@ -9,3 +18,16 @@ PagerDutyServiceConfig = namedtuple('PagerDutyServiceConfig', ('aws_account_name
                                                                'service_name',
                                                                'ses_console_url',
                                                                'ses_reputation_dashboard_url'))
+'''
+class:PagerDutyServiceConfig
+
+Args:
+    aws_account_name (str): AWS account name. Ex: supercoolco.
+    aws_environment (str): AWS environment name. Ex: prod.
+    aws_region (str): AWS region. Ex: us-west-2.
+    events_url (str): The Pager Duty events url to publish to.
+    routing_key (str): The Pager Duty routing key.
+    service_name (str): The name of the service. Ex: lambda-ses-account-monitor.
+    ses_console_url (str): The SES console url.
+    ses_reputation_dashboard_url (str): The SES reputation dashboard url.
+'''
