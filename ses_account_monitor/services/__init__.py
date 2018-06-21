7527717de20082eb34f80a5c17a098ee33a16b4c
Mike Chau <mikechau@users.noreply.github.com>
2018-06-20 22:45:03 -0700

    Prepare for release

--- ses_account_monitor/services/__init__.py
+++ ses_account_monitor/services/__init__.py
@@ -5,4 +5,4 @@ from ses_account_monitor.services.pager_duty_service import PagerDutyService
 from ses_account_monitor.services.ses_service import SesService
 from ses_account_monitor.services.slack_service import SlackService
 
-__all__ = ['CloudwatchService', 'PagerDutyService', 'SesService', 'SlackService']
+__all__ = ['CloudWatchService', 'PagerDutyService', 'SesService', 'SlackService']
